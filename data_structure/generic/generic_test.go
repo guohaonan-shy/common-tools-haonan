bc3f00e0eb98d09b98cd115344fa3e3f54b3a6b6
Aatrox <guohaonan.980421@163.com>
2023-02-20 17:00:40 +0800

    [feat]新增case

--- data_structure/generic/generic_test.go
+++ data_structure/generic/generic_test.go
@@ -18,3 +18,50 @@ func Test_ContainString(t *testing.T) {
 	assert.Equal(t, true, res)
 	assert.Equal(t, false, res2)
 }
+
+func Test_ContainByFunc(t *testing.T) {
+	// int64, string, struct that all fields are comparable
+	fn := func(value int64) bool {
+		if value == 1 {
+			return true
+		}
+		return false
+	}
+
+	res1 := ContainsByFunc(fn, []int64{1, 2, 3})
+	res2 := ContainsByFunc(fn, []int64{2, 3, 4})
+	assert.Equal(t, true, res1, "should contain 1, list:%d", []int64{2, 2, 3})
+	assert.Equal(t, false, res2, "shouldn't contain 1")
+
+	// struct
+
+	type testStruct struct {
+		value string
+		age   int64
+	}
+
+	s := []*testStruct{
+		{
+			value: "yes",
+			age:   10,
+		},
+		{
+			value: "no",
+			age:   21,
+		},
+		{
+			value: "ghn",
+			age:   32,
+		},
+	}
+
+	fn1 := func(object *testStruct) bool {
+		if object.value == "ghn" {
+			return true
+		}
+		return false
+	}
+
+	res3 := ContainsByFunc(fn1, s)
+	assert.Equal(t, true, res3, "should contain ghn")
+}
