5d2d23905f1ea5fd8979161ff6b8467ea9996ee5
Aatrox <guohaonan.980421@163.com>
2023-02-20 15:53:49 +0800

    [feat]test case

--- data_structure/generic/generic_test.go
+++ data_structure/generic/generic_test.go
@@ -7,5 +7,14 @@ import (
 
 func Test_ContainInt64(t *testing.T) {
 	res := Contains[int64](int64(1), []int64{1, 2, 3})
+	res2 := Contains[int64](int64(1123121), []int64{1, 2, 3})
 	assert.Equal(t, true, res)
+	assert.Equal(t, false, res2)
+}
+
+func Test_ContainString(t *testing.T) {
+	res := Contains("abc", []string{"abc", "def", "efs"})
+	res2 := Contains("a", []string{"b", "c", "d"})
+	assert.Equal(t, true, res)
+	assert.Equal(t, false, res2)
 }
