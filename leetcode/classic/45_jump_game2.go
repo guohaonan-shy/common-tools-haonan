7586cf80bac132ada23a57558b1e14133a17e564
Aatrox <guohaonan@Guos-MacBook-Pro.local>
2023-12-26 16:43:33 +0800

    [feat]jump

---
+++ leetcode/classic/45_jump_game2.go
@@ -0,0 +1,14 @@
+package classic
+
+func jump(nums []int) int {
+	max_pos, end := 0, 0
+	step := 0
+	for i := 0; i < len(nums)-1; i++ {
+		max_pos = max(max_pos, i+nums[i])
+		if i == end { // 目前以遍历到上一步能走的最远距离，需要跨到下一个最大距离
+			end = max_pos
+			step++
+		}
+	}
+	return step
+}
---
+++ leetcode/classic/55_jump_game1.go
@@ -0,0 +1,14 @@
+package classic
+
+// 当某个位置x能抵达，意味着在x之前的位置，至少存在一个y，y可达且y+nums[y] >= x
+func canJump(nums []int) bool {
+	max_distance := 0
+	for pos, distance := range nums {
+		if pos <= max_distance { // can reach
+			max_distance = max(max_distance, pos+distance)
+		} else {
+			break
+		}
+	}
+	return max_distance >= len(nums)-1
+}
