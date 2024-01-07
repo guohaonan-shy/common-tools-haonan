62ae019b1f16dc2ee6c0625cb0fe09338bd27dcd
Aatrox <guohaonan980421@163.com>
2024-01-07 17:17:29 +0800

    [feat]

--- leetcode/classic/section/228_summary_ranges.go
+++ leetcode/classic/section/228_summary_ranges.go
@@ -5,26 +5,19 @@ import "strconv"
 func summaryRanges(nums []int) []string {
 
 	res := make([]string, 0)
-	left, right := 0, 0
-	for right < len(nums) {
-		if right-left != nums[right]-nums[left] {
-			var str string
-			if right-1 == left {
-				str = strconv.Itoa(nums[left])
-			} else {
-				str = strconv.Itoa(nums[left]) + "->" + strconv.Itoa(nums[right-1])
-			}
-			res = append(res, str)
-			left = right
-		} else {
-			right++
-		}
-	}
+	right := 0
 
-	if right-1 == left {
-		res = append(res, strconv.Itoa(nums[left]))
-	} else {
-		res = append(res, strconv.Itoa(nums[left])+"->"+strconv.Itoa(nums[right-1]))
+	for right < len(nums) {
+		left := right
+		for right++; right < len(nums) && nums[right-1]+1 == nums[right]; right++ {
+
+		}
+
+		str := strconv.Itoa(nums[left])
+		if left < right-1 {
+			str += "->" + strconv.Itoa(nums[right-1])
+		}
+		res = append(res, str)
 	}
 
 	return res
