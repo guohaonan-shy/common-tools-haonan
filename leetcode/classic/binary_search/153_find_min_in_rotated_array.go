c01c8ef243e17c3be79b121bc0a2f9588043b6a6
Aatrox <guohaonan980421@163.com>
2024-01-23 09:44:49 +0800

    [feat]

--- leetcode/classic/binary_search/153_find_min_in_rotated_array.go
+++ leetcode/classic/binary_search/153_find_min_in_rotated_array.go
@@ -6,20 +6,26 @@ func findMin(nums []int) int {
 
 func binarySearchMinRotate(nums []int, left, right int) int {
 
-	for left < right {
+	for left < right { // 至少有两个元素
 		// 单调
 		//if nums[left] <= nums[right] {
 		//	return nums[left]
 		//}
 
-		mid := (left + right) / 2
+		mid := (left + right) / 2 // 靠左倾斜
 		if nums[mid] < nums[right] {
 			right = mid
 		} else {
 			left = mid + 1
 		}
+		// 按照靠左倾斜计算，则会出现当left = right -1时，会出现自己比较自己
+		//if nums[mid] >= nums[left] {
+		//	left = mid + 1
+		//} else {
+		//	right = mid
+		//}
 
 	}
 
-	return nums[left]
+	return nums[(left+right)/2]
 }
