f68d66027f9fb39f6631862a17ae5b56af8cedaa
Aatrox <guohaonan980421@163.com>
2024-01-26 12:38:11 +0800

    [feat]

--- leetcode/every_day/688_knight_probability.go
+++ leetcode/every_day/688_knight_probability.go
@@ -64,7 +64,7 @@ func knightProbability(n int, k int, row int, column int) float64 {
 }
 
 func knightProbability_dp(n int, k int, row int, column int) float64 {
-	dp := make([][][]float64, k+1)
+	dp := make([][][]float64, k+1) // 定义为第x步，棋子在(y,z)的概率
 
 	for step := range dp {
 		dp[step] = make([][]float64, n)
@@ -72,13 +72,12 @@ func knightProbability_dp(n int, k int, row int, column int) float64 {
 			dp[step][rowIdx] = make([]float64, n)
 			for columnIdx := range dp[step][rowIdx] {
 				if step == 0 {
-					dp[step][rowIdx][columnIdx] = 1
+					dp[step][rowIdx][columnIdx] = 1 // 第0步，如果k=0,则随便一个坐标概率都是1
 				} else {
 					for _, dir := range directions {
 						newX, newY := rowIdx+dir[0], columnIdx+dir[1]
-
-						if newX >= 0 && newX < n && newY >= 0 && newY < n {
-							dp[step][rowIdx][columnIdx] += dp[step-1][newX][newY] / 8
+						if newX >= 0 && newX < n && newY >= 0 && newY < n { // 起点不在棋盘内，则概率为0，即不加
+							dp[step][rowIdx][columnIdx] += dp[step-1][newX][newY] / 8 // 乘1/8，是因为8个方向随机选择
 						}
 					}
 				}
