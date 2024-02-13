e73fa04bf20868465020a027481ad2546c4afcc9
Aatrox <guohaonan980421@163.com>
2024-02-13 11:57:44 +0800

    [feat]

--- leetcode/classic/back_track/79_word_search.go
+++ leetcode/classic/back_track/79_word_search.go
@@ -1,13 +1,16 @@
 package back_track
 
+var dirs = [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
 func exist(board [][]byte, word string) bool {
 	var dfs func(i, j int, idx int) bool
 	dfs = func(i, j int, idx int) bool {
 
-		if idx == len(word) {
+		if idx == len(word) { // match finished
 			return true
 		}
 
+		// board[i][j] == '# means this element has been covered in this branch
 		if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]) || board[i][j] == '#' {
 			return false
 		}
@@ -19,22 +22,13 @@ func exist(board [][]byte, word string) bool {
 		temp := board[i][j]
 		board[i][j] = '#'
 
-		if dfs(i-1, j, idx+1) {
-			return true
+		for _, dir := range dirs {
+			dx, dy := i+dir[0], j+dir[1]
+			if dfs(dx, dy, idx+1) {
+				return true
+			}
 		}
-
-		if dfs(i+1, j, idx+1) {
-			return true
-		}
-
-		if dfs(i, j-1, idx+1) {
-			return true
-		}
-
-		if dfs(i, j+1, idx+1) {
-			return true
-		}
-
+		// four direction is not match, it means that the word[idx+1:] doesn't match with target
 		board[i][j] = temp
 		return false
 	}
