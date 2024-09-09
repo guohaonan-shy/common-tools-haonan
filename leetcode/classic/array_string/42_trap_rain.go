7de504f26a34dcd2df35ebc59b5cdf805232d1ea
Aatrox <guohaonan980421@163.com>
2024-09-09 22:15:57 +0800

    feat: comment for monotonous stack

--- leetcode/classic/array_string/42_trap_rain.go
+++ leetcode/classic/array_string/42_trap_rain.go
@@ -34,17 +34,27 @@ func trap_stack(height []int) int {
 	total := 0
 	for i := 0; i < len(height); i++ {
 
+		// len(stack) > 0 => has top of stack; height[stack[len(stack)-1]] < height[i] => bottom exists
 		for len(stack) > 0 && height[i] > height[stack[len(stack)-1]] {
 			top := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 			if len(stack) == 0 {
 				break
-			}
+			} // no decreased order on ith
+			// the second smallest element in the decreased array
 			left := stack[len(stack)-1]
-
+			/*
+				width = i-1-left
+				height = min(height[left], height[i]) - height[top]
+			*/
 			water := (i - 1 - left) * (min(height[left], height[i]) - height[top])
 			total += water
 		}
+		/*
+			two scenarios for pushing stack:
+			1. stack's length == 0
+			2. decrease order; it means the top of stack is the local minimum of decreased order
+		*/
 		stack = append(stack, i)
 	}
 	return total
