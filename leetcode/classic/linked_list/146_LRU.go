59cbd2e7b1224adccc4e6b7d1216b70ccf3201a6
Aatrox <haonan.guo@bytedance.com>
2024-09-19 12:24:30 +0800

    feat: double dummy

--- leetcode/classic/linked_list/146_LRU.go
+++ leetcode/classic/linked_list/146_LRU.go
@@ -17,7 +17,7 @@ func (node *LinkedNode) tail() *LinkedNode {
 	cur := node
 	for ; cur.Next != nil; cur = cur.Next {
 	}
-	return cur
+	return cur.Prev
 }
 
 func (node *LinkedNode) search(key int) *LinkedNode {
@@ -35,13 +35,21 @@ type LRUCacheIns struct {
 }
 
 func Constructor(capacity int) *LRUCacheIns {
+	// set dummy in the head and tail of the linked-list
+	dummyHead, dummyTail := &LinkedNode{Key: -1, Val: -1}, &LinkedNode{Key: -1, Val: -1}
+	dummyHead.Next = dummyTail
+	dummyTail.Prev = dummyHead
+
 	return &LRUCacheIns{
 		Capacity:   capacity,
 		data:       make(map[int]int, capacity),
-		linkedList: nil,
+		linkedList: dummyHead,
 	}
 }
 
+/*
+Get() needs to move the retrieved kv node into the head of linked list
+*/
 func (lru *LRUCacheIns) Get(key int) int {
 
 	val, ok := lru.data[key]
@@ -52,54 +60,46 @@ func (lru *LRUCacheIns) Get(key int) int {
 	// move the retrieved element into the head
 	target := lru.linkedList.search(key)
 	prev, next := target.Prev, target.Next
+	// because of the existence of two dummy nodes, target.prev && target.next must be non-nil
 	// 1. delete the connection
-	if prev != nil {
-		prev.Next = next
-	}
-	if next != nil {
-		next.Prev = prev
-	}
-	// 2. change the connection of target to head
-	if len(lru.data) == 1 {
-		return val
-	}
+	prev.Next = next
+	next.Prev = prev
 
-	second := lru.linkedList
-	target.Prev = second.Prev
-	target.Next = second
-	second.Prev = target
-	lru.linkedList = target
+	// 2. insert 'target' into the head
+	oldHead := lru.linkedList.Next
+	target.Next = oldHead
+	oldHead.Prev = target
+
+	lru.linkedList.Next = target
+	target.Prev = lru.linkedList
 	return val
 }
+
+/*
+	Put() needs to insert the node in the head of linked-list;
+	And if the capacity is full, we have to remove the tail of this linked-list
+*/
 
 func (lru *LRUCacheIns) Put(key, val int) {
 	// no elements:
 	// 1. init
-	// 2. after a series of operations, there is no data currently.
-	if lru.linkedList == nil {
-		lru.linkedList = NewLinkedNode(key, val)
-		lru.data[key] = val
-		return
-	}
+	// 2. after a series of operations, there is no data currently
 
-	// if key is in the data, we don't need to remove the tail node
+	// if key is in the data, we don't need to remove the tail node => just insert to the head
 	if _, ok := lru.data[key]; ok {
 		target := lru.linkedList.search(key)
 		target.Val = val
 		prev, next := target.Prev, target.Next
-		if prev != nil {
-			prev.Next = nil
-		}
-		if next != nil {
-			next.Prev = nil
-		}
 
-		second := lru.linkedList
-		if second != target { // not head, avoid self reference
-			target.Next = second
-			second.Prev = target
-			lru.linkedList = target
-		}
+		prev.Next = next
+		next.Prev = prev
+
+		oldHead := lru.linkedList.Next
+		lru.linkedList.Next = target
+		target.Next = oldHead
+		oldHead.Prev = target
+		target.Prev = lru.linkedList
+
 		lru.data[key] = val
 		return
 	}
@@ -107,27 +107,28 @@ func (lru *LRUCacheIns) Put(key, val int) {
 	// insert a kv that doesn't in the list
 	// insert element in the head of list,
 	// but there is a corner case: we need to remove the tail if reach the capacity
-	currentLength := len(lru.data)
-	if currentLength == lru.Capacity { // need to remove tail
-		//
-		tail := lru.linkedList.tail() // previous node become the tail, o(n)
+
+	if lru.Capacity == len(lru.data) {
+		tail := lru.linkedList.tail()
+
+		prev, next := tail.Prev, tail.Next
+		prev.Next = next
+		next.Prev = prev
+
 		delete(lru.data, tail.Key)
-		preTail := tail.Prev
-		if preTail != nil {
-			preTail.Next = nil
-		} else {
-			// capacity == 1
-			lru.linkedList = nil
-		}
-		tail.Prev = nil
 	}
-	insertedNode := NewLinkedNode(key, val)
-	secondNode := lru.linkedList
-	insertedNode.Next = secondNode
-	if secondNode != nil {
-		secondNode.Prev = insertedNode
+
+	insertedNode := &LinkedNode{
+		Key: key,
+		Val: val,
 	}
+	oldHead := lru.linkedList.Next
+
+	lru.linkedList.Next = insertedNode
+	insertedNode.Next = oldHead
+	oldHead.Prev = insertedNode
+	insertedNode.Prev = lru.linkedList
+
 	lru.data[key] = val
-	lru.linkedList = insertedNode
 	return
 }
