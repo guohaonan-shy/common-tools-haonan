936be80828675d92e08b2d6bc3a3efe8c0e847ba
Aatrox <guohaonan980421@163.com>
2024-09-20 22:09:58 +0800

    feat: 129 v2

--- leetcode/classic/binary_tree/129_sum_path.go
+++ leetcode/classic/binary_tree/129_sum_path.go
@@ -26,3 +26,40 @@ func findPath(root *TreeNode, prev []int, total *int) {
 		findPath(root.Right, append(prev, root.Val), total)
 	}
 }
+
+var (
+	total int
+)
+
+func sumNumbersV2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	prefix := make([]int, 0)
+	sumProcessor(root.Left, append(prefix, root.Val))
+	sumProcessor(root.Right, append(prefix, root.Val))
+	return total
+}
+
+func sumProcessor(root *TreeNode, prefix []int) {
+	if root.Left == nil && root.Right == nil {
+		// leaf node
+		number := 0
+		prefix = append(prefix, root.Val)
+		for i := 0; i < len(prefix); i++ {
+			number = number*10 + prefix[i]
+		}
+		//number = number*10 + root.Val
+		total += number
+		return
+	}
+
+	if root.Left != nil {
+		sumProcessor(root.Left, append(prefix, root.Val))
+	}
+
+	if root.Right != nil {
+		sumProcessor(root.Right, append(prefix, root.Val))
+	}
+	return
+}
--- leetcode/classic/binary_tree/binary_tree_test.go
+++ leetcode/classic/binary_tree/binary_tree_test.go
@@ -142,7 +142,7 @@ func Test_129(t *testing.T) {
 	} {
 
 		t.Run(tc.input, func(t *testing.T) {
-			res := sumNumbers(treeify(tc.input))
+			res := sumNumbersV2(treeify(tc.input))
 			assert.Equal(t, tc.expected, res)
 		})
 
