36594b7df60541ea3c8976f23ded8b8825c047c7
Aatrox <haonan.guo@bytedance.com>
2024-09-23 17:18:52 +0800

    feat:74

--- leetcode/classic/binary_search/74_search_2d_matrix.go
+++ leetcode/classic/binary_search/74_search_2d_matrix.go
@@ -30,3 +30,43 @@ func binarySearch(nums []int, left, right int, target int) int {
 		return binarySearch(nums, mid+1, right, target)
 	}
 }
+
+func searchMatrixV2(matrix [][]int, target int) bool {
+	for i := 0; i < len(matrix); i++ {
+		row := matrix[i]
+		find, idx := binarySearchV2(row, 0, len(row)-1, target)
+		if find {
+			return true
+		}
+
+		if idx >= len(row) {
+			continue
+		}
+
+		if idx == 0 {
+			return false
+		}
+	}
+	return false
+}
+
+func binarySearchV2(nums []int, left, right, target int) (bool, int) {
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return true, mid
+		} else if target < nums[mid] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	if nums[left] == target {
+		return true, left
+	} else if nums[left] < target {
+		return false, left + 1
+	} else {
+		return false, left
+	}
+}
