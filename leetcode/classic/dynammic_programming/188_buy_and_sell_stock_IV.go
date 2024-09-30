4a2f008b6ac92217dc1e5b93d38c46e1d0965050
Aatrox <haonan.guo@bytedance.com>
2024-09-30 17:35:44 +0800

    feat: k transactions

--- leetcode/classic/dynammic_programming/188_buy_and_sell_stock_IV.go
+++ leetcode/classic/dynammic_programming/188_buy_and_sell_stock_IV.go
@@ -33,3 +33,38 @@ func maxProfit_general(k int, prices []int) int {
 	}
 	return profits
 }
+
+func maxProfitV2_kTransactions(k int, prices []int) int {
+	dp := make([][]int, len(prices)+1)
+	for i := range dp {
+		dp[i] = make([]int, 2*k+1)
+	}
+
+	for day := 1; day <= len(prices); day++ {
+		for status := 0; status <= day && status < 2*k+1; status++ {
+
+			if status == 0 {
+				dp[day][0] = dp[day-1][0]
+				continue
+			}
+
+			profit := prices[day-1]
+			if status%2 == 1 {
+				profit = -profit
+			}
+
+			if day == status {
+				dp[day][status] = dp[day-1][status-1] + profit
+			} else {
+				dp[day][status] = max(dp[day-1][status], dp[day-1][status-1]+profit)
+			}
+
+		}
+	}
+
+	maxVal := 0
+	for i := 0; i < 2*k+1; i++ {
+		maxVal = max(maxVal, dp[len(prices)][i])
+	}
+	return maxVal
+}
