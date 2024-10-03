99df09201157891a68c457890765b9621f4961dc
Aatrox <guohaonan980421@163.com>
2024-10-03 22:24:35 +0800

    feat: optimized

--- leetcode/every_day/166_fraction_to_recurring_decimal.go
+++ leetcode/every_day/166_fraction_to_recurring_decimal.go
@@ -3,9 +3,14 @@ package every_day
 import "strconv"
 
 func fractionToDecimal(numerator int, denominator int) string {
+	// can divide with no remain
+	if numerator%denominator == 0 {
+		return strconv.Itoa(numerator / denominator)
+	}
+
 	// 1. generate the sign
 	res := ""
-	if numerator*denominator < 0 {
+	if (numerator < 0) != (denominator < 0) { // this writing style seems like expression
 		res += "-"
 	}
 	// all the steps are same with calculation between two positive number
@@ -21,29 +26,22 @@ func fractionToDecimal(numerator int, denominator int) string {
 	integer := numerator / denominator
 	numerator = numerator % denominator
 
-	if numerator == 0 {
-		return res + strconv.Itoa(integer)
-	}
-
 	res += strconv.Itoa(integer) + "."
-	posOfDot := len(res)
 
 	set := make(map[int]int, 0)
 
-	for numerator != 0 {
+	for numerator != 0 && set[numerator] == 0 {
+		set[numerator] = len(res)
 		numerator *= 10
-		if last, ok := set[numerator]; ok {
-			// reconstruct in repeated
-			res = res[:last] + "(" + res[last:] + ")"
-			break
-		}
-
 		digit := numerator / denominator
 		res += strconv.Itoa(digit)
-		set[numerator] = posOfDot
-
-		posOfDot++
 		numerator = numerator % denominator
+	}
+
+	if set[numerator] != 0 {
+		last := set[numerator]
+		// reconstruct in repeated
+		res = res[:last] + "(" + res[last:] + ")"
 	}
 	return res
 }
