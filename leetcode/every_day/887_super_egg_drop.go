8d8afd5b4ff208855cb4a275c65abc6f139585d8
Aatrox <guohaonan980421@163.com>
2024-10-14 21:15:35 +0800

    feat: explaination

--- leetcode/every_day/887_super_egg_drop.go
+++ leetcode/every_day/887_super_egg_drop.go
@@ -3,7 +3,7 @@ package every_day
 import "math"
 
 /*
-timeout => time complexity: O(n^2*k)
+	timeout => time complexity: O(n^2*k)
 */
 func superEggDrop(k int, n int) int {
 	dp := make([][]int, n+1) // the first dimension is the number of floors
@@ -51,3 +51,20 @@ func superEggDrop(k int, n int) int {
 	}
 	return dp[n][k]
 }
+
+/*
+	Finally, let us make a conclusion about this solution:
+
+	After we decided to use dynamic programming to solve this problem, we try to abstract the sub problem that is similar with the problem.
+	First - analyze the problem and split into sub problem:
+		We have n floors and k eggs, supposed we try to drop an egg to shrink the scope, we have two cases:
+			1. if the egg is broken, it means that the threshold is in [0, t-1], and now we have k-1 eggs => sub problem: we have k-1 eggs to check under t-1 floor
+			2. if not, it means that the threshold is in [t, n], and now we still have k eggs => sub problem: we have k eggs to check under n-t floor
+	Second - optimize the time complexity:
+		if we iterate by n and k, then try from 1 to current highest floor, the time complexity is O(n^2*k) => timeout
+		we know:
+			1. dp[n-t][eggs] is decreasing as t increases
+			2. dp[t-1][eggs-1] is increasing as t increases
+		when we try the drop floor from 1 to n, the maximum of dp[n-t][eggs] and dp[t-1][eggs-1] is the moves we can determine with certainty
+		therefore, the position that are closer to the mid is possible to minimum value => binary search to find the minimum value
+*/
