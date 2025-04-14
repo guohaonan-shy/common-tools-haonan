f624b6a885a332ef3d3381ef67f2e74c10b71584
Aatrox <guohaonan980421@163.com>
2025-04-14 23:29:29 +0800

    feat: water

--- leetcode/classic/two_pointer/11_container_with_most_water.go
+++ leetcode/classic/two_pointer/11_container_with_most_water.go
@@ -5,8 +5,13 @@ func maxArea(height []int) int {
 	max_area := 0
 	for left < right {
 		max_area = max(max_area, (right-left)*min(height[left], height[right]))
-		if height[left] < height[right] { // 因为左右双指针无论谁移动，矩阵的长肯定是减小的，要想面积增大，唯一的可能就是改变宽，宽增大的唯一方法就是左右指针所指元素的较小值（宽）增加
-			left++ // 假如right--，那么面积一定是减小的(height[right-1]如果仍然是偏大的，即宽仍是height[left]; 反之宽是比height[left]更小的值);left++面积可能会增加
+		if height[left] < height[right] {
+			// 因为左右双指针无论谁移动，矩阵的长肯定是减小的，要想面积增大，唯一的可能就是改变宽，宽增大的唯一方法就是左右指针所指元素的较小值（宽）增加
+			// 当height[right]大时，即容器盛水面积由较小的height[left]决定
+			// 假如right--，那么面积一定是减小的(height[right-1]无论相比于height[right]是减小的还是增大的，宽仍是height[left] 或者 是比height[left]更小的值);
+			// left++是有可能增加面积的
+			// 从left和right 两个指针本身考虑
+			left++
 		} else { // 反之
 			right--
 		}
